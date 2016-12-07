3de4948d5c92bd0270461218a6dda2dd3179174f
Nicholas Lofton <nick.lofton@angieslist.com>
2016-12-07 11:29:14 -0500

    Updating generator

--- generator.py
+++ generator.py
@@ -5,20 +5,20 @@ movement = {}
 movement = {
     0: {
       'a1': {
-        'min': -0.7,
-        'max': -1.1
+        'min': -0.9374,
+        'max': -0.9260
       },
       'a2': {
-        'min': -0.25,
-        'max': 0.0
+        'min': -0.1963,
+        'max': -0.1261
       },
       'a3': {
-        'min': 0.0,
-        'max': 0.4
+        'min': 0.0693,
+        'max': 0.2824
       },
       'a4': {
-        'min': -0.1,
-        'max': 0.3
+        'min': -0.0359,
+        'max': 0.1113
       },
       'signalPower': 0.4821,
       'outputs': {
@@ -32,20 +32,20 @@ movement = {
     },
     1: {
       'a1': {
-        'min': -1.9,
-        'max': -1.3
+        'min': -1.6957,
+        'max': -1.4096
       },
       'a2': {
-        'min': 0.0,
-        'max': 0.5
+        'min': 0.0333,
+        'max': 0.3154
       },
       'a3': {
-        'min': 0.2,
-        'max': 0.8
+        'min': 0.3277,
+        'max': 0.5890
       },
       'a4': {
-        'min': -0.25,
-        'max': 0.1
+        'min': -0.2003,
+        'max': 0.0602
       },
       'signalPower': 0.8901,
       'outputs': {
@@ -59,20 +59,20 @@ movement = {
     },
     2: {
       'a1': {
-        'min': -2,
-        'max': -1.5
+        'min': -1.8022,
+        'max': -1.6591
       },
       'a2': {
-        'min': 0.25,
-        'max': 0.5
+        'min': 0.3128,
+        'max': 0.4710
       },
       'a3': {
-        'min': 0.45,
-        'max': 0.8
+        'min': 0.4905,
+        'max': 0.6052
       },
       'a4': {
-        'min': -0.3,
-        'max': -0.1
+        'min': -0.2632,
+        'max': -0.0814
       },
       'signalPower': 0.9488,
       'outputs': {
@@ -86,20 +86,20 @@ movement = {
     },
     3: {
       'a1': {
-        'min': -2.3,
-        'max': -1.8
+        'min': -2.1035,
+        'max': -1.9456
       },
       'a2': {
-        'min': 0.5,
-        'max': 1.0
+        'min': 0.6327,
+        'max': 0.9701
       },
       'a3': {
-        'min': 0.3,
-        'max': 0.8
+        'min': 0.4601,
+        'max': 0.6688
       },
       'a4': {
-        'min': -0.17,
-        'max': -0.25
+        'min': -0.2899,
+        'max': -0.2875
       },
       'signalPower': 1.1469,
       'outputs': {
@@ -113,20 +113,20 @@ movement = {
     },
     4: {
       'a1': {
-        'min': -2.4,
-        'max': -2
+        'min': -2.2974,
+        'max': -2.1922
       },
       'a2': {
-        'min': 1.0,
-        'max': 1.7
+        'min': 1.1591,
+        'max': 1.5157
       },
       'a3': {
-        'min': 0.2,
-        'max': 0.43
+        'min': 0.2509,
+        'max': 0.4010
       },
       'a4': {
-        'min': -0.32,
-        'max': -0.09
+        'min': -0.2969,
+        'max': -0.1108
       },
       'signalPower': 1.3278,
       'outputs': {
@@ -140,20 +140,20 @@ movement = {
     },
     5: {
       'a1': {
-        'min': -2.9,
-        'max': -2.1
+        'min': -2.5220,
+        'max': -2.3579
       },
       'a2': {
-        'min': 1.45,
-        'max': 2.1
+        'min': 1.5370,
+        'max': 1.9656
       },
       'a3': {
-        'min': -0.34,
-        'max': 0.1
+        'min': -0.2942,
+        'max': 0.0802
       },
       'a4': {
-        'min': -0.3,
-        'max': -0.12
+        'min': -0.2552,
+        'max': -0.1471
       },
       'signalPower': 1.5206,
       'outputs': {
@@ -171,27 +171,76 @@ filename = "generatedTrainingData.txt"
 
 target = open(filename, 'w')
 target.truncate()
+
+target.write("fuzzifier: 4\n")
+target.write("errorMargin: 0.01\n")
+target.write("iterationLimit: 2000\n")
 target.write("topology: 5 6 6\n")
-for currentMovementIndex in range (0, 12):
+target.write("learningRate: 0.95\n")
+target.write("momentum: 0.1\n")
+
+count = 0
+while count <= 4800:
+    count += 1
+
+    currentMovementIndex = random.randint(0, 5)
     currentMovement = movement[currentMovementIndex % 6]
-    for x in range (0, 3600):
-        target.write("in: %f %f %f %f %f\n" \
-              % (
-                  random.uniform(currentMovement['a1']['min'], currentMovement['a1']['max']),
-                  random.uniform(currentMovement['a2']['min'], currentMovement['a2']['max']),
-                  random.uniform(currentMovement['a3']['min'], currentMovement['a3']['max']),
-                  random.uniform(currentMovement['a4']['min'], currentMovement['a4']['max']),
-                  currentMovement['signalPower']
-              ))
-        target.write("out: %f %f %f %f %f %f\n" \
-                     % (
-                         currentMovement['outputs'][0],
-                         currentMovement['outputs'][1],
-                         currentMovement['outputs'][2],
-                         currentMovement['outputs'][3],
-                         currentMovement['outputs'][4],
-                         currentMovement['outputs'][5],
-                     ))
+#for x in range(0, 4800):
+#    for currentMovementIndex in range(0, 6):
+        #currentMovement = movement[currentMovementIndex % 6]
+
+    target.write("in: %f %f %f %f %f\n" \
+                 % (
+                     random.uniform(currentMovement['a1']['min'], currentMovement['a1']['max']),
+                     random.uniform(currentMovement['a2']['min'], currentMovement['a2']['max']),
+                     random.uniform(currentMovement['a3']['min'], currentMovement['a3']['max']),
+                     random.uniform(currentMovement['a4']['min'], currentMovement['a4']['max']),
+                     currentMovement['signalPower']
+                 ))
+    target.write("out: %f %f %f %f %f %f\n" \
+                 % (
+                     currentMovement['outputs'][0],
+                     currentMovement['outputs'][1],
+                     currentMovement['outputs'][2],
+                     currentMovement['outputs'][3],
+                     currentMovement['outputs'][4],
+                     currentMovement['outputs'][5],
+                 ))
+
+
+target.close()
+
+filename = "liveTestingData.txt"
+
+target = open(filename, 'w')
+target.truncate()
+count = 0
+
+while count < 10000:
+    count += 1
+
+    currentMovementIndex = random.randint(0, 5)
+    currentMovement = movement[currentMovementIndex % 6]
+    target.write("in: %f %f %f %f %f\n" \
+        % (
+              random.uniform(currentMovement['a1']['min'], currentMovement['a1']['max']),
+              random.uniform(currentMovement['a2']['min'], currentMovement['a2']['max']),
+              random.uniform(currentMovement['a3']['min'], currentMovement['a3']['max']),
+              random.uniform(currentMovement['a4']['min'], currentMovement['a4']['max']),
+              currentMovement['signalPower']
+          )
+    )
+
+    target.write("out: %f %f %f %f %f %f\n" \
+        % (
+              currentMovement['outputs'][0],
+              currentMovement['outputs'][1],
+              currentMovement['outputs'][2],
+              currentMovement['outputs'][3],
+              currentMovement['outputs'][4],
+              currentMovement['outputs'][5],
+          )
+     )
 
 
 target.close()
